234d7d80c29583f957f00d7ab7b50b5c6866fe34
tanaka-0224 <r.iku0224@icloud.com>
2024-03-06 12:24:19 +0900

    test push

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import StartPage from "./components/StartPage";
 
-const App = () => {
-  return (
-    <>
-    <h1>HelloWorld</h1>
-    </>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <StartPage />
+  </React.StrictMode>
+);
