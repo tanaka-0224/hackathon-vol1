61632fa5a61fab30b38916a9a0ef7c5183a670fc
h1ne <henryharlay@gmail.com>
2024-03-07 10:57:27 +0900

    Initial commit

---
+++ src/main/java/com/example/echolingo/echolingo/EcholingoApplication.java
@@ -0,0 +1,13 @@
+package com.example.echolingo.echolingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcholingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcholingoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/echolingo/echolingo/EcholingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.echolingo.echolingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcholingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
