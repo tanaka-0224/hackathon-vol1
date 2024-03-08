e3448d0ad6e1f5ed92981e7876c0f3753031e6d9
h1ne <henryharlay@gmail.com>
2024-03-09 03:48:57 +0900

    Add class, constructor, method to generate new map with random numbers

--- src/main/java/com/example/echolingo/echolingo/service/MapData.java
+++ src/main/java/com/example/echolingo/echolingo/service/MapData.java
@@ -1,10 +1,17 @@
 package com.example.echolingo.echolingo.service;
 
 import java.lang.reflect.WildcardType;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 public class MapData {
     static final int WIDTH = 5;
     static int[][] map = new int[WIDTH][WIDTH];
+
+    public void MapData() {
+        mapInitialize();
+    }    
 
     public void mapInitialize() {
         for (int i = 0; i < WIDTH; i++) {
@@ -12,5 +19,25 @@ public class MapData {
                 map[i][j] = 0;
             }
         }
+
+        Set<Integer> selectedNumbers = new HashSet<>();
+        Random random = new Random();
+
+        int count = 0;
+        while (selectedNumbers.size() < WIDTH * WIDTH) {
+            int number = 1 + random.nextInt(65);
+            if (selectedNumbers.add(number)) {
+                map[count / WIDTH][count % WIDTH] = number;
+                count++;
+            }
+        }
+
+        // Display this matrix map
+        // for (int i = 0; i < WIDTH; i++) {
+        //     for (int j = 0; j < WIDTH; j++) {
+        //         System.out.print(map[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
     }
 }
