4c95b39fd49428af685d42b8d49027e378c5b4ef
h1ne <henryharlay@gmail.com>
2024-03-09 03:48:02 +0900

    Add code about initializing new map

--- src/main/java/com/example/echolingo/echolingo/service/GameLogic.java
+++ src/main/java/com/example/echolingo/echolingo/service/GameLogic.java
@@ -1,5 +1,11 @@
 package com.example.echolingo.echolingo.service;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class GameLogic {
-    
+    // Create MapData instance and INITIALIZE new map filled with rundom numbers automatically with its constructor.
+    MapData mapData = new MapData();
+
 }
