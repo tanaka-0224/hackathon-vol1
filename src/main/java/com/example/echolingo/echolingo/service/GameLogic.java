e372022cab6daaac3a7a25081f02b6167a671e94
h1ne <henryharlay@gmail.com>
2024-03-09 03:52:13 +0900

    Delete unneeded imports

--- src/main/java/com/example/echolingo/echolingo/service/GameLogic.java
+++ src/main/java/com/example/echolingo/echolingo/service/GameLogic.java
@@ -1,9 +1,5 @@
 package com.example.echolingo.echolingo.service;
 
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
-
 public class GameLogic {
     // Create MapData instance and INITIALIZE new map filled with rundom numbers automatically with its constructor.
     MapData mapData = new MapData();
