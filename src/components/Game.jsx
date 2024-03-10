c461b3183e13c12fc99cc5e43275773b8f9a636a
liuyi <782527347@qq.com>
2024-03-10 14:55:04 +0900

    add new function!

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,253 +2,321 @@ import React, { useState, useEffect, useRef } from "react";
 import CountDown from "./CountDown";
 import Popup from "./Popup";
 import "../types/Visual.css";
-import async from "async";
 
 const Game = () => {
-  const [randomSlangs, setRandomSlangs] = useState([]);
-  const [selectedSlang, setSelectedSlang] = useState("Select slang");
-  const [error, setError] = useState(null);
-  const [cellColors, setCellColors] = useState([]);
-  const [defaultcellColors, setDefaultCellColors] = useState("#FFFFFF");
-  const [isPopupVisible, setPopupVisible] = useState(true);
-  const [isTimerRunning, setTimerRunning] = useState(false);
-  const [selectnumber, setSelectNumber] = useState(0);
-  const [colorCounts, setColorCounts] = useState({});
-  const [isCardRotated, setIsCardRotated] = useState(false); // ボタンの回転状態
-  const [tips, setTips] = useState("");
-
-  const gridRef = useRef([]);
-
-  const GPT_CHAT_URI = `${process.env.REACT_APP_GPT_API_URL}/gpt/process_slang`;
-  const handleCardRotate = () => {
-    setIsCardRotated(!isCardRotated); // ボタンを回転させる状態に設定
-  };
-
-  useEffect(() => {
-    // 色の数が変更されたときの処理
-    if (colorCounts["#db605f"] === 12) {
-      // 画面遷移させる処理
-      window.location.href = "/resultwin";
-    }
-  }, [colorCounts]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/slang.txt");
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
+    const [randomSlangs, setRandomSlangs] = useState([]);
+    const [selectedSlang, setSelectedSlang] = useState("Select slang");
+    const [error, setError] = useState(null);
+    const [cellColors, setCellColors] = useState([]);
+    const [defaultcellColors, setDefaultCellColors] = useState("#FFFFFF");
+    const [isPopupVisible, setPopupVisible] = useState(true);
+    const [isTimerRunning, setTimerRunning] = useState(false);
+    const [selectnumber, setSelectNumber] = useState(0);
+    const [colorCounts, setColorCounts] = useState({});
+    const gridRef = useRef([]);
+    const [sendSlangs, setSendSlangs] = useState([]);
+    const [tips, setTips] = useState(""); // 提示信息状态
+    const GPT_CHAT_URI = `${process.env.REACT_APP_GPT_API_URL}/gpt/scene_chat`;
+    useEffect(() => {
+        // 色の数が変更されたときの処理
+        if (colorCounts["#db605f"] === 12) {
+            // 画面遷移させる処理
+            window.location.href = "/resultwin";
         }
-        const textData = await response.text();
-        const slangArray = textData
-          .split("\n")
-          .map((line) => line.trim())
-          .filter(Boolean);
+    }, [colorCounts]);
 
-        const colors = Array(12)
-          .fill("#db605f")
-          .concat(Array(12).fill("#20a095"))
-          .concat(["#4a4e4d"]);
-        // 色の順番をシャッフルする
-        colors.sort(() => Math.random() - 0.5);
-        setCellColors(colors);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("/slang.txt");
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                const textData = await response.text();
+                const slangArray = textData
+                    .split("\n")
+                    .map((line) => line.trim())
+                    .filter(Boolean);
 
-        // 初期のランダムな背景色を保持
+                const colors = Array(12)
+                    .fill("#db605f")
+                    .concat(Array(12).fill("#20a095"))
+                    .concat(["#4a4e4d"]);
+                // 色の順番をシャッフルする
+                colors.sort(() => Math.random() - 0.5);
+                setCellColors(colors);
 
-        const randomIndexes = [];
-        while (randomIndexes.length < 25) {
-          const randomIndex = Math.floor(Math.random() * slangArray.length);
-          if (!randomIndexes.includes(randomIndex)) {
-            randomIndexes.push(randomIndex);
-          }
-        }
+                // 初期のランダムな背景色を保持
 
-        const selectedSlangs = randomIndexes.map((index) => slangArray[index]);
-        setRandomSlangs(selectedSlangs);
-        // 背景色の出現回数をカウント
-        const counts = {};
-        randomSlangs.forEach((slang, index) => {
-          const color = cellColors[index];
-          counts[color] = (counts[color] || 0) + 1;
+                const randomIndexes = [];
+                while (randomIndexes.length < 25) {
+                    const randomIndex = Math.floor(Math.random() * slangArray.length);
+                    if (!randomIndexes.includes(randomIndex)) {
+                        randomIndexes.push(randomIndex);
+                    }
+                }
+
+                const selectedSlangs = randomIndexes.map((index) => slangArray[index]);
+                setRandomSlangs(selectedSlangs);
+                // 背景色の出現回数をカウント
+                const counts = {};
+                randomSlangs.forEach((slang, index) => {
+                    const color = cellColors[index];
+                    counts[color] = (counts[color] || 0) + 1;
+                });
+                setColorCounts(counts);
+            } catch (error) {
+                setError(error);
+            }
+        };
+
+        fetchData();
+        gridRef.current.forEach((row, rowIndex) => {
+            row.forEach((cell, colIndex) => {
+                const backgroundColor = window.getComputedStyle(cell).backgroundColor;
+                console.log(
+                    `Gridのセル(${rowIndex + 1}, ${colIndex + 1})の背景色:`,
+                    backgroundColor
+                );
+            });
         });
-        setColorCounts(counts);
-      } catch (error) {
-        setError(error);
-      }
+    }, []);
+
+    useEffect(() => {
+        const filteredSlangs = randomSlangs.reduce((acc, slang, index) => {
+            if (cellColors[index] === "#20a095") {
+                return [...acc, slang];
+            }
+            return acc;
+        }, []);
+
+        setSendSlangs(filteredSlangs);
+    }, [randomSlangs, cellColors]);
+
+    // 异步获取提示信息
+    useEffect(() => {
+        const fetchTips = async () => {
+            try {
+                const response = await fetch("/gpt/scene_chat");
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                const data = await response.json();
+                console.log(data);
+                setTips(data.hint_message);
+            } catch (error) {
+                console.error("Error fetching tips:", error);
+                setTips(""); // 如果获取失败，将提示信息清空或设置为默认值
+            }
+        };
+
+        fetchTips();
+    }, [selectedSlang]); // 仅当 selectedSlang 改变时执行
+
+    console.log(sendSlangs);
+
+    const handlePopupStartButtonClick = () => {
+        // ここで初期のランダムな背景色を使って何かの処理を行うことができます
+
+        setPopupVisible(false);
+        setTimerRunning(true);
     };
 
-    fetchData();
-    gridRef.current.forEach((row, rowIndex) => {
-      row.forEach((cell, colIndex) => {
-        const backgroundColor = window.getComputedStyle(cell).backgroundColor;
-        console.log(
-          `Gridのセル(${rowIndex + 1}, ${colIndex + 1})の背景色:`,
-          backgroundColor
-        );
-      });
-    });
-  }, []);
+    const handleStartButtonClick = () => {
+        setPopupVisible(true);
+    };
 
-  const handlePopupStartButtonClick = () => {
-    // ここで初期のランダムな背景色を使って何かの処理を行うことができます
+    const handlePopupClose = () => {
+        setPopupVisible(false);
+        setTimerRunning(true);
+    };
 
-    setPopupVisible(false);
-    setTimerRunning(true);
-  };
+    const [gridRotations, setGridRotations] = useState(Array(25).fill(false));
+    const [transparentGrids, setTransparentGrids] = useState([]);
 
-  const handleStartButtonClick = () => {
-    setPopupVisible(true);
-  };
+    const handleSlangClick = async (slang, rowIndex, columnIndex) => {
+        const selectedGridIndex = rowIndex * 5 + columnIndex + 1;
 
-  const handlePopupClose = () => {
-    setPopupVisible(false);
-    setTimerRunning(true);
-  };
-
-    const handleSlangClick = async (slang, rowIndex) => {
-        const str1 = rowIndex;
-        const result1 = Number(str1);
-
-        // selectnumberを更新
-        setSelectNumber(result1 + 1);
-
-        // 変更
+        setSelectNumber(rowIndex * 5 + columnIndex + 1);
         setSelectedSlang(slang);
 
+        // 发送请求到 API 获取相关信息
         try {
             const response = await fetch(GPT_CHAT_URI, {
-                method: 'POST', // 请确认这里是POST请求还是GET请求，根据你的后端API来决定
+                method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ slang: slang }), // 将所选的slang作为JSON对象的属性发送到后端
+                body: JSON.stringify({ input_sentences: slang }), // 发送当前单词到 API
             });
 
             if (!response.ok) {
                 throw new Error("Network response was not ok");
             }
+
             const data = await response.json();
-            // 假设返回的数据中包含名为tips的字段
-            setTips(data.hint_message); // 更新tips状态
+
+            // 更新 UI 显示相关信息
+            setTips(data.chat_message);
         } catch (error) {
             console.error('Error fetching tips:', error);
-            // 如果获取失败，将tips设置为空字符串或其他默认值
+            // 如果获取失败，将提示信息设置为空字符串或其他默认值
             setTips('');
+        }
+
+        // 選択されたGridだけを回転させる
+        const updatedRotations = gridRotations.map((rotation, index) =>
+            index === selectedGridIndex - 1 ? !rotation : rotation
+        );
+
+        setGridRotations(updatedRotations);
+
+        const isRotated = gridRotations[rowIndex * 5 + columnIndex];
+
+        if (!isRotated) {
+            setGridRotations(updatedRotations);
+        }
+
+        setTransparentGrids((prevTransparentGrids) => [
+            ...prevTransparentGrids,
+            selectedGridIndex,
+        ]);
+
+        setTimeout(() => {
+            setTransparentGrids((prevTransparentGrids) =>
+                prevTransparentGrids.filter(
+                    (gridIndex) => gridIndex !== selectedGridIndex
+                )
+            );
+        }, 1000);
+    };
+    const handleButtonPress = (rowIndex, columnIndex) => {
+        setDefaultCellColors((prevColors) => {
+            const newColors = [...prevColors];
+            newColors[selectnumber - 1] = cellColors[selectnumber - 1];
+
+            const counts = {};
+            newColors.forEach((color) => {
+                counts[color] = (counts[color] || 0) + 1;
+            });
+            setColorCounts(counts);
+
+            const selectedGridIndex = rowIndex * 5 + columnIndex + 1;
+
+            const updatedRotations = gridRotations.map((rotation, index) =>
+                index === selectedGridIndex - 1 ? !rotation : rotation
+            );
+
+            setGridRotations(updatedRotations);
+
+            return newColors;
+        });
+
+        if (cellColors[selectnumber - 1] === "#4a4e4d") {
+            window.location.href = "/resultlose";
         }
     };
 
-    const handleButtonPress = () => {
-        setDefaultCellColors((prevColors) => {
-      const newColors = [...prevColors];
-      newColors[selectnumber - 1] = cellColors[selectnumber - 1];
-
-      const counts = {};
-      newColors.forEach((color) => {
-        counts[color] = (counts[color] || 0) + 1;
-      });
-      setColorCounts(counts);
-
-      return newColors;
-    });
-   if (cellColors[selectnumber - 1] === "#4a4e4d") {
-      window.location.href = "/resultlose";
+    if (error) {
+        return <div>Error: {error.message}</div>;
     }
-  };
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+    const gridRows = [];
+    for (let i = 0; i < 5; i++) {
+        const start = i * 5;
+        const end = start + 5;
+        const row = randomSlangs.slice(start, end);
+        gridRows.push(row);
+    }
 
-  const gridRows = [];
-  for (let i = 0; i < 5; i++) {
-    const start = i * 5;
-    const end = start + 5;
-    const row = randomSlangs.slice(start, end);
-    gridRows.push(row);
-  }
+    return (
+        <div>
+            {/* スタートボタン */}
+            <div className="pressme" style={{ textAlign: "center",fontSize: "40px", marginTop: "30px"}}onClick={handleStartButtonClick}>Porse</div>
 
-  return (
-    <div>
-      {/* スタートボタン ・ポーズボタン*/}
-      <div className="pressme" style={{ textAlign: "center",fontSize: "40px", marginTop: "30px"}}onClick={handleStartButtonClick}>Porse</div>
+            {/* ポップアップ */}
+            {isPopupVisible && (
+                <Popup
+                    onClose={handlePopupClose}
+                    onStartButtonClick={handlePopupStartButtonClick}
+                />
+            )}
 
-      {/* ポップアップ */}
-      {isPopupVisible && (
-        <Popup
-          onClose={handlePopupClose}
-          onStartButtonClick={handlePopupStartButtonClick}
-        />
-      )}
-      <div style={{ display: 'flex' }}>
-      <div className="selected">
-        <h1>{selectedSlang && <p style={{fontSize: "40px"}}>Selected Slang: {selectedSlang}</p>}</h1>
-      </div>
-      <div className="balloonSlang" style={{textAlign:"center",fontSize:40}}>
-        slang number
-      </div>
-      </div>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <div className="cards"
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(5, 1fr)",
-          gap: "10px",
-        }}
-      >
-        {gridRows.map((row, rowIndex) =>
-          row.map((slang, columnIndex) => (
-            <div
-              className={isCardRotated ? 'rotate' : ''} // isRotatingの状態によってクラスを適用する
-              key={rowIndex * 5 + columnIndex}
-              style={{
-                border: "1px solid #ccc",
-                padding: "10px",
-                fontSize:20,
-                textAlign: "center",
-                cursor: "pointer",
-                backgroundColor:
-                  selectedSlang === slang
-                    ? "#e0e0e0"
-                    : defaultcellColors[rowIndex * 5 + columnIndex],
-              }}
-              onClick={() =>{
-                handleSlangClick(slang, rowIndex * 5 + columnIndex)
-                handleCardRotate();
-              }}
-            >
-              <span>{rowIndex * 5 + columnIndex + 1}</span> {/* 番号を表示 */}
-              {slang}
+            <div style={{ display: 'flex' }}>
+                <div className="selected">
+                    <h1>{selectedSlang && <p style={{fontSize: "40px"}}>Selected Slang: {selectedSlang}</p>}</h1>
+                </div>
+                <div className="balloonSlang" style={{textAlign:"center",fontSize:40}}>
+                    slang number
+                </div>
             </div>
-          ))
-        )}
-      </div>
-
-      <div style={{ marginLeft: '20px', marginTop: '30px' }} >
-        <img src="./images/ringo.png" className="slangmaster" alt="slang master"/>
-        <p style={{ fontSize:40, textAlign:"center",marginTop: '30px'}}>Slang Master</p>
-      </div>
-      </div>
-      <div style={{ display: 'flex' }}>
-      <div className="pressme"
-        style={{
-          marginTop: "30px",
-          padding: "10px",
-          fontSize: "40px",
-          textAlign: "center"
-        }}
-       onClick={handleButtonPress}
-      >
-        Press me!
-      </div>
-      <div className="balloonTips" style={{textAlign:"center", fontSize:20}}>
-          <p>{tips}</p>
-      </div>
-      </div>
-      {/* カウントダウンタイマー */}
-      {isTimerRunning && <CountDown seconds={600000} />}
-    </div>
-  );
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <div className="cards"
+                     style={{
+                         display: "grid",
+                         gridTemplateColumns: "repeat(5, 1fr)",
+                         gap: "10px",
+                     }}
+                >
+                    {gridRows.map((row, rowIndex) =>
+                        row.map((slang, columnIndex) => (
+                            <div
+                                className={
+                                    gridRotations[rowIndex * 5 + columnIndex] ? "rotate" : ""
+                                }
+                                key={rowIndex * 5 + columnIndex + 1}
+                                style={{
+                                    border: "1px solid #ccc",
+                                    padding: "10px",
+                                    textAlign: "center",
+                                    cursor: "pointer",
+                                    transform: gridRotations[rowIndex * 5 + columnIndex]
+                                        ? "rotateY(180deg)"
+                                        : "none",
+                                    color: transparentGrids.includes(rowIndex * 5 + columnIndex + 1)
+                                        ? "transparent"
+                                        : "inherit",
+                                    transition: "transform 0.5s",
+                                    backgroundColor:
+                                        selectedSlang === slang
+                                            ? "#e0e0e0"
+                                            : defaultcellColors[rowIndex * 5 + columnIndex],
+                                }}
+                                onClick={() => {
+                                    handleSlangClick(slang, rowIndex, columnIndex);
+                                }}
+                            >
+                                <span>{rowIndex * 5 + columnIndex + 1}</span>
+                                {slang}
+                            </div>
+                        ))
+                    )}
+                </div>
+                <div style={{ marginLeft: '20px', marginTop: '30px' }} >
+                    <img src="./images/ringo.png" className="slangmaster" alt="slang master"/>
+                    <p style={{ fontSize:40, textAlign:"center",marginTop: '30px'}}>Slang Master</p>
+                </div>
+            </div>
+            <div style={{ display: 'flex' }}>
+                <div className="pressme"
+                     style={{
+                         marginTop: "30px",
+                         padding: "10px",
+                         fontSize: "40px",
+                         textAlign: "center"
+                     }}
+                     onClick={handleButtonPress}
+                >
+                    Press me!
+                </div>
+                <div className="balloonTips" style={{textAlign: "center", fontSize: 20}}>
+                    <p>{tips}</p>
+                </div>
+            </div>
+            {/* 倒计时 */}
+            {isTimerRunning && <CountDown seconds={600000} />}
+        </div>
+    );
 };
 
 export default Game;
-
